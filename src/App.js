46900a484bf16c994abb8af266f5a22d245861cd
Lucija Filipovic <lucija_filipovic_@hotmail.com>
2023-02-02 15:26:56 +0100

    GitHub request to fetch data about commits

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+//import axios from 'axios';
+import axios from './Request'
 
 function App() {
+  const [commits, setCommits] = useState([]);
+
+  useEffect(() => {
+    axios.get(`repos/lufilipovic/react-calendar-app/commits`)
+      .then(response => {
+        setCommits(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
-    <div className="App">
+    <div>
+      {commits.map(commit => (
+        <div key={commit.sha}>
+          <p>{commit.commit.author.name}</p>
+          <p>{commit.commit.message}</p>
+          <p>{commit.commit.author.date}</p>
+          <p>{commit.commit.author.email}</p>
+          <br></br>
+        </div>
+      ))}
     </div>
   );
 }
