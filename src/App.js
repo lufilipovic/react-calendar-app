6bed9703f3717a342be2c799868162bd8fca01eb
Lucija Filipovic <lucija_filipovic_@hotmail.com>
2023-02-02 21:27:02 +0100

    Adding react-big-calendar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import { useCommits } from './Request';
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import moment from 'moment'
 
 function App() {
   const commits = useCommits();
+  const localizer = momentLocalizer(moment)
 
   return (
     <div>
+      <Calendar
+        localizer={localizer}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500, margin: "50px" }} />
+
       {commits.map(commit => (
         <div key={commit.sha}>
           <p>{commit.commit.author.name}</p>
